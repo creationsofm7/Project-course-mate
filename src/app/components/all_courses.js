d9855195fee24f26fd705236bbbe9f7a9cf8cbd0
GHOST . <muditpanday17@gmail.com>
2024-04-21 15:16:44 +0530

    fixed router not defined error

--- src/app/components/all_courses.js
+++ src/app/components/all_courses.js
@@ -10,7 +10,7 @@ import { useState, useEffect, useContext } from "react";
 export default function All_courses() {
   
 
-  router = useRouter();
+  const router = useRouter();
 
   const [list, setList] = useState([]);
 
